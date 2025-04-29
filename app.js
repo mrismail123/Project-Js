6e977b143b51349936fd7e9f679d4f85c4c732c4
Nouhailabarrak2 <nouhaila.barrak@etu.uae.ac.ma>
2025-04-29 23:33:19 +0100

    commencer backend fichiers

---
+++ app.js
@@ -0,0 +1,18 @@
+// Import the Express library
+const express = require('express');
+
+// Create an instance of an Express app
+const app = express();
+
+// Set the port number
+const PORT = 3000;
+
+// Define a route for the homepage
+app.get('/', (req, res) => {
+    res.send('Welcome to the Exam Platform!');
+});
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
